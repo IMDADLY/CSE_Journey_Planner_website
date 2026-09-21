0d7fec57219caaf7ee34411348174cf0a7331456
Imdadurahman Imran <helloimdadu@gmail.com>
2026-09-22 01:02:48 +0530

    Remove unncessary imports

--- backend/src/main/java/com/example/cseplanner/repository/SpecializationRepository.java
+++ backend/src/main/java/com/example/cseplanner/repository/SpecializationRepository.java
@@ -3,7 +3,6 @@ package com.example.cseplanner.repository;
 import com.example.cseplanner.models.Specialization;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface SpecializationRepository extends MongoRepository<Specialization, String> {
